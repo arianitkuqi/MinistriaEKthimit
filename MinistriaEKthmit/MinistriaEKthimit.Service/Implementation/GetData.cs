3083b4b6a89b9ceb07880ed8d0043fb5f16fde3b
BesartKuleta <besart.kuleta@techvision-ks.com>
2017-10-04 13:46:25 +0200

    new changes

---
+++ MinistriaEKthmit/MinistriaEKthimit.Service/Implementation/GetData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FactoryAdapter;
+
+namespace MinistriaEKthimit.Service.Implementation
+{
+    public class GetData
+    {
+
+        FactoryAdapter.FactoryAdapter fac = new FactoryAdapter.FactoryAdapter("dasd");
+
+        public GetData()
+        {
+
+        }
+    }
+}
