cd24388eb03480980b236c0d82afbb9adb65c320
BesartKuleta <besart.kuleta@techvision-ks.com>
2017-10-10 16:47:32 +0200

    Register Controller

---
+++ MinistriaEKthimit/Controllers/ApplicationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MinistriaEKthimit.Controllers
+{
+    [Authorize]
+    public class ApplicationController : Controller
+    {
+        // GET: Application
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ MinistriaEKthimit/Scripts/MyScripts/Common.js
@@ -0,0 +1,26 @@
+﻿(function () {
+
+    requestToken = (function (user, pw) {
+        $.ajax({
+            url: '/token',
+            type: 'POST',
+            contentType: 'application/json',
+            data: {
+                userName: "bbesart-k@msn.com",
+                password: "Tech@test123",
+                grant_type: 'password'
+            },
+            success: function (response) {
+                sessionStorage.setItem('accessToken', response.acces_token)
+                console.log(response)
+            }
+        })
+    })();
+
+    return {
+        Authenticate: this.requestToken
+    }
+
+})()
+
+
