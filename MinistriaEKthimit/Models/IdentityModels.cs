bd1905ff0f14132bd109c050d6a4557023f249ef
BesartKuleta <besart.kuleta@techvision-ks.com>
2017-10-19 11:27:31 +0200

    cs

--- MinistriaEKthimit/Models/IdentityModels.cs
+++ MinistriaEKthimit/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace MinistriaEKthimit.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("MinistriaKthimit", throwIfV1Schema: false)
         {
         }
 
